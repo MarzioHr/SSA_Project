c8b71061ac7ab980514bbfbc9cd3e14e11626ed6
Lewle <sebastian.senev@gmail.com>
2022-02-28 10:14:24 +0100

    Update smart_thermostat.py
    update publish function.

--- smart_thermostat.py
+++ smart_thermostat.py
@@ -1,14 +1,22 @@
 import paho.mqtt.client as mqtt
 import time
+import certifi
+from random import uniform
+
 
 # Constants #
-BROKER = "127.0.0.1" # Use External Broker
+BROKER = "test.mosquitto.org" # Use External Broker
+#BROKER = "mosquitto.ssa-project.xyz" # Use External Broker
+#PORT = 1884
+#USER = "thermostat"
+#PASS = "123456789"
+
 
 # Main Class #
 class Thermostat:
     def __init__(self, label):
         self.state = "OFF"
-        self.temp = 20.0
+        self.temp = 0
         self.label = label
         self.client = mqtt.Client("Smart_Thermometer_"+str(self.label))
         self.root_topic = f"thermostat/{self.label}"
@@ -19,7 +27,12 @@ class Thermostat:
         return True
 
     def publish_temp(self):
-        self.client.publish(f"{self.root_topic}/temp", self.temp)
+        result = self.client.publish(f"{self.root_topic}/temp", self.temp)
+        status = result[0]
+        if status == 0:
+            print(f"Send `{temprature}` to topic `{self.root_topic}/temp`")
+        else:
+            print(f"Failed to send message to topic {self.root_topic}/temp`")
         return True
 
     def publish_alive(self):
@@ -44,9 +57,20 @@ def on_connect(client, userdata, flags, rc):
 # Main Loop #
 label = input("Where is this Thermometer located? ").replace(" ", "")
 thermostat = Thermostat(label)
+print("Root Topic:", thermostat.root_topic)
+print("connection Topic:", thermostat.conn_topic)
 thermostat.client.on_connect = on_connect
+#thermostat.client.username_pw_set(username=USER,password=PASS)
+#thermostat.client.tls_set(certifi.where())
+#thermostat.client.connect(BROKER, port=PORT)
 thermostat.client.connect(BROKER)
 thermostat.client.will_set(thermostat.conn_topic, 0, qos=0, retain=True) #set will
 
 thermostat.publish_alive()
-thermostat.client.loop_forever()
+# thermostat.client.loop_forever()
+
+while True:
+    time.sleep(1)
+    temprature = uniform(20, 28)
+    thermostat.subscribe_change_temp(temprature)
+    thermostat.publish_temp()
